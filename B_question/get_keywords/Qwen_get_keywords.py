89c7e5668da328a3592ed88a3807ec63eaa73d58
zhihui-shao <121287193+zhihui-shao@users.noreply.github.com>
2023-12-01 10:19:14 +0800

    12.1

--- B_question/get_keywords/Qwen_get_keywords.py
+++ B_question/get_keywords/Qwen_get_keywords.py
@@ -6,7 +6,7 @@ import re
 def Qwen_post(question):
     
     # 定义API接口的URL
-    url = 'http://172.31.233.206:8000/v1/chat/completions'
+    url = 'http://172.31.233.204:8000/v1/chat/completions'
 
     # 定义请求的JSON数据
     data = {
@@ -16,16 +16,18 @@ def Qwen_post(question):
                 "role": 'user',
                 "content": f'''
                         这是关键词提取器，你要从用户输入的文本中提取关键词。
-                        首先分析句子的组成结构，然后提取出完整的主语，谓语，宾语，时间名词，地点名词等等。
-                        如果句子中出现了时间，则关键词一定要包括表示时间的名词和公司名称。
+                        关键词一定是句子中包含的原词，通常是名词或句子的宾语。
+                        如：净利润、部门、主营业务、实际控制人、注册资本、发明专利、英文名称、原材料、毛利率、营业收入、在册员工、
+                        主要产品、法定代表人、子公司、有效期、所处行业、在册员工、发起人、控股股东、增长幅度，收入比重、销售额、营业收入、
+                        人工成本、业务成本。
                         关键词可以有多个，每个关键字之间用“、”隔开。
                         输出完毕后结束，不要生成新的用户输入，不要新增内容。
                         
-                        示例：                     
+                        示例模版：                     
                         用户输入：云南沃森生物技术股份有限公司负责产品研发的是什么部门？
                         云南沃森生物技术股份有限公司、产品研发、部门
-                        用户输入：本次发行前，大连派思燃气系统股份有限公司的控股股东持有公司的多少股份？占公司总股份的多少？
-                        本次发行前、大连派思燃气系统股份有限公司、控股股东、总股份
+                        用户输入：海南双成药业股份有限公司的前身是什么？成立于何时？
+                        海南双成药业股份有限公司、前身、成立
                         用户输入：截至2016年6月30日，华达汽车科技股份有限公司及子公司共有技术研发人员多少人？技术研发人占员工总人数的多大比例？
                         2016年6月30日、华达汽车科技股份有限公司、子公司、技术研发人员、员工总人数
                         用户输入：各报告期末，江苏旷达汽车织物集团股份有限公司存货分别为多少万元？占流动资产的比例分别多少？
@@ -38,6 +40,8 @@ def Qwen_post(question):
                         深圳市超频三科技股份有限公司、主要产品、未来业务增长、重点产品
                         用户输入：截至2009年12月31日，兰州海默科技股份有限公司的正式在册员工人数是多少？
                         2009年12月31日、兰州海默科技股份有限公司、正式在册员工
+                        用户输入：截至2020年12月31日，湖南长远锂科股份有限公司的控股股东是谁？通过何种方式持有公司多少股权？
+                        2020年12月31日、湖南长远锂科股份有限公司、控股股东、股权
 
                         ###
                         用户输入：{question}
@@ -46,10 +50,10 @@ def Qwen_post(question):
             }
         ],
         "do_sample": True,
-        "temperature": 0,
+        "temperature": 0.1,
         "top_p": 0,
         "n": 1,
-        "max_tokens": 2048,
+        "max_tokens": 4096,
         "stream": False
     }
 
@@ -73,16 +77,18 @@ with open(file_path, "r", encoding="utf-8") as file:
 for obj in datas: 
     id = obj['b_id']     
     question = obj['b_question']
-    match = False
-    while not match:
-        answer = Qwen_post(question)
-        print(answer)
-        pattern = r'、' 
-        match = re.search(pattern, answer)
-        
-    output_file = "B_keywords_1129.json"    # 每次循环迭代都将结果追加到JSON文件
-    with open(output_file, "a", encoding="utf-8") as output:
-        json.dump({"b_id":id , "b_question": question,"keywords":answer}, output, ensure_ascii=False,indent=4)
-        output.write(','+'\n')
+    if id > 437:
+        match = False
+        while not match:
+            answer = Qwen_post(question)
+            print(id)
+            print(answer)
+            pattern = r'、' 
+            match = re.search(pattern, answer)
+            
+        output_file = "B_keywords_1130.json"    # 每次循环迭代都将结果追加到JSON文件
+        with open(output_file, "a", encoding="utf-8") as output:
+            json.dump({"b_id":id , "b_question": question,"keywords":answer}, output, ensure_ascii=False,indent=4)
+            output.write(','+'\n')
         
     
