2bc5e6d9543e7eed326bf1bc6fe3b718ce5233b3
陈泓龙 <chenhonglong@szltech.com>
2023-12-02 00:13:02 +0800

    微调数据集

--- A_question/sql_finetune/adjust_data.py
+++ A_question/sql_finetune/adjust_data.py
@@ -4,7 +4,7 @@ from itertools import groupby
 
 
 def adjust():
-    json_file = './finetune_sql_1201_chl_1.json'
+    json_file = './finetune_sql_1201.json'
     with open(json_file, "r", encoding="utf-8") as file:
         datas = json.load(file)
 
@@ -14,22 +14,28 @@ def adjust():
             obj["question"] = text_list[1].strip().replace('#', '').replace('\n', '')
         obj["question"] = obj["question"].strip().replace('#', '').replace('\n', '')
 
-    with open(json_file, "w", encoding="utf-8") as output:
+    output_file = './finetune_sql_1202.json'
+    with open(output_file, "w", encoding="utf-8") as output:
         json.dump(datas, output, ensure_ascii=False, indent=4)
 
 
 def statis():
-    source_sql = './finetune_sql_1201_chl_1.json'
+    source_sql = './finetune_sql_1202.json'
     with open(source_sql, "r", encoding="utf-8") as file:
         source_datas = json.load(file)
 
     data_length = len(source_datas)
 
-    for i in range(0, data_length):
-        for j in range(i, data_length):
-            if i == j:
+    list_i = list(range(0, data_length - 1))
+    for i in list_i:
+        list_j = list(range(i + 1, data_length))
+        for j in list_j:
+            if source_datas[i] == source_datas[j]:
+                print(1)
                 source_datas.pop(j)
                 data_length = data_length - 1
+                list_i.pop()
+                list_j.pop()
 
     with open(source_sql, 'w', encoding='utf-8') as f:
         json.dump(source_datas, f, ensure_ascii=False, indent=4)
