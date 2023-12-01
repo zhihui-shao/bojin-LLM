ceaa9e2e09c42ecab575fd6bd0d2cd1c7af37590
陈泓龙 <chenhonglong@szltech.com>
2023-12-01 10:52:10 +0800

    生成最终数据

---
+++ answer_1201/jsonl2json.py
@@ -0,0 +1,20 @@
+import json
+
+
+def jsonl_to_json(jsonl_file, json_file):
+    json_data = []
+    with open(jsonl_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            try:
+                obj = json.loads(line)
+                json_data.append(obj)
+            except json.JSONDecodeError:
+                print(f"Ignoring invalid JSON object: {line}")
+
+    with open(json_file, 'w', encoding='utf-8') as f:
+        json.dump(json_data, f, ensure_ascii=False, indent=4)
+
+
+jsonl_file = './answer_1126_kw.jsonl'
+json_file = './answer_1126_kw.json'
+jsonl_to_json(jsonl_file, json_file)
